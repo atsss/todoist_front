ca5a7a19ee1797c392a8ae1bd6321344d12d841f
atsushi0623itoh <atsushi0623itoh@gmail.com>
2019-04-12 09:40:18 +0900

    create Check component

--- src/components/molecules/TaskList/index.js
+++ src/components/molecules/TaskList/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from './styles.module.sass'
 import Txt from '../../atoms/Txt'
+import Check from '../../atoms/Check'
 
 const TaskList = ({ time, name, className }) => (
   <li className={ [ styles.root, className ].join(' ') }>
-    <Txt weight='bold'>{ time }</Txt>
+    <Check />
+    <Txt weight='bold' className='u-ml15'>{ time }</Txt>
     <Txt className='u-ml5'>{ name }</Txt>
   </li>
 )
