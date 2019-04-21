d2e68a257ea9ce94098a836bd664ef394bf1ab33
atsushi0623itoh <atsushi0623itoh@gmail.com>
2019-04-21 20:45:11 +0900

    rename TxtLink component to Link

---
+++ src/components/atoms/Link/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link as RouterLink } from 'react-router-dom'
+import styles from './styles.module.sass'
+
+const Link = ({ to, className, children }) => (
+  <div className={ className }>
+    <RouterLink to={to} className={styles.link}>
+      { children }
+    </RouterLink>
+  </div>
+)
+
+export default Link
--- src/components/organisms/Menu/index.js
+++ src/components/organisms/Menu/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import TxtLink from '../../atoms/TxtLink'
+import Link from '../../atoms/Link'
 import Txt from '../../atoms/Txt'
 
 const Menu = ({ className }) => (
   <div className={ className }>
-    <TxtLink to='/'>
+    <Link to='/'>
       <Txt weight='bold'>今日</Txt>
-    </TxtLink>
-    <TxtLink to='/tasks/new' className='u-mt10'>
+    </Link>
+    <Link to='/tasks/new' className='u-mt10'>
       <Txt weight='bold'>新規作成</Txt>
-    </TxtLink>
+    </Link>
   </div>
 )
 
