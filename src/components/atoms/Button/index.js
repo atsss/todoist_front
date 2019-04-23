3d54cb0469861c62c4aff4837200889ee6caf25d
atsushi0623itoh <atsushi0623itoh@gmail.com>
2019-04-23 10:27:42 +0900

    create Button component

---
+++ src/components/atoms/Button/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './styles.module.sass'
+import { WhiteTxt } from '../../atoms/Txt'
+
+const Button = ({ text, onClick, className }) => (
+  <button
+    className={ [ styles.button, className ].join(' ') }
+    onClick={(e) => onClick(e)}
+  >
+
+    <WhiteTxt weight='bold'>{ text }</WhiteTxt>
+  </button>
+)
+
+export default Button
--- src/components/templates/TaskNewTemplate/index.js
+++ src/components/templates/TaskNewTemplate/index.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import styles from '../../atoms/Input/styles.module.sass'
+import Button from '../../atoms/Button'
+import PageTitle from '../../molecules/PageTitle'
 
 const TaskNewTemplate = ({ className }) => (
   <div className={className}>
-    new
+    <PageTitle title='新規作成' />
+    <input
+      className={styles.input}
+      type='text'
+      placeholder='新しいタスク'
+    />
+    <input
+      className={styles.input}
+      type='text'
+      placeholder='日付'
+    />
+    <input
+      className={styles.input}
+      type='text'
+      placeholder='時間'
+    />
+    <Button
+      text='タスクを追加'
+      onClick={(b) => console.log(b)}
+    />
   </div>
 )
 
