6783ab8d282bfd2678168dbc5d63b44312ecef99
atsushi0623itoh <atsushi0623itoh@gmail.com>
2019-05-28 09:47:52 +0900

    create done task mutation in component

--- src/components/molecules/TaskList/index.js
+++ src/components/molecules/TaskList/index.js
@@ -4,10 +4,27 @@ import Check from '../../atoms/Check'
 import Link from '../../atoms/Link'
 import Txt from '../../atoms/Txt'
 import Settings from '../../molecules/Settings'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+
+const DONE_TASK_MUTATION = gql`
+  mutation DoneTaskMutation($id: ID!) {
+    doneTask(id: $id) {
+      id
+      name
+      dueDate
+    }
+  }
+`
 
 const TaskList = ({ id, time, name, className }) => (
   <li className={ [ styles.root, className ].join(' ') }>
-    <Check />
+    <Mutation
+      mutation={DONE_TASK_MUTATION}
+      variables={{ id }}
+    >
+      {doneTaskMutation => <div onClick={doneTaskMutation}> <Check /> </div> }
+    </Mutation>
     <Txt weight='bold' className='u-ml15'>{ time }</Txt>
     <Txt className='u-ml5'>{ name }</Txt>
     <Link to={`/tasks/${id}/edit`}>
