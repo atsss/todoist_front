ca2db0185a6d8ce086d4ff903d0ee4ddf577206f
atsushi0623itoh <atsushi0623itoh@gmail.com>
2019-07-06 19:34:53 +0900

    tmp

--- src/components/templates/Login/index.js
+++ src/components/templates/Login/index.js
@@ -11,18 +11,36 @@ const LoginTemplate = ({ className }) => {
   let session = SessionContainer.useContainer()
 
   return (
-    <Mutation
-      mutation={ LOGIN_MUTATION }
-      variables={{ email, password }}
-      onCompleted={ (data) => {
-        if(data.signinUser == null) { return }
+    <div>
+      <div className="flex flex-column mt3">
+        <input
+          className="mb2"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          type="text"
+          placeholder="A description for the link"
+        />
+        <input
+          className="mb2"
+          value={password}
+          onChange={e => setEmail(e.target.password)}
+          type="text"
+          placeholder="The URL for the link"
+        />
+      </div>
+      <Mutation
+        mutation={ LOGIN_MUTATION }
+        variables={{ email, password }}
+        onCompleted={ (data) => {
+          if(data.signinUser == null) { return }
 
-        document.cookie = `token=${data.signinUser.token}`
-        session.login()
-      }}
-    >
-      { loginMutation => <Button text='ログイン' onClick={ loginMutation } /> }
-    </Mutation>
+          document.cookie = `token=${data.signinUser.token}`
+          session.login()
+        }}
+      >
+        { loginMutation => <Button text='ログイン' onClick={ loginMutation } /> }
+      </Mutation>
+    </div>
   )
 }
 
