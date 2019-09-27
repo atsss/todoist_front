e02c0c8b8dd3d1a4a426a83923fb09ba19026494
atsushi0623itoh <atsushi0623itoh@gmail.com>
2019-09-27 20:19:45 +0900

    use functinal component in TaskIndexPage

--- src/components/pages/TaskIndexPage.js
+++ src/components/pages/TaskIndexPage.js
@@ -1,21 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import TaskIndexTemplate from "../templates/TaskIndexTemplate";
 import { Query } from "react-apollo";
 import { TASKS_QUERY } from "../../queries";
 
-export default class TaskIndexPage extends Component {
-  render() {
-    return (
-      <Query query={TASKS_QUERY}>
-        {({ loading, error, data }) => {
-          if (loading) return <div>Fetching</div>;
-          if (error) return <div>Error</div>;
+const TaskIndexPage = () => {
+  return (
+    <Query query={TASKS_QUERY}>
+      {({ loading, error, data }) => {
+        if (loading) return <div>Fetching</div>;
+        if (error) return <div>Error</div>;
 
-          const { tasks } = data;
+        const { tasks } = data;
 
-          return <TaskIndexTemplate tasks={tasks} />;
-        }}
-      </Query>
-    );
-  }
-}
+        return <TaskIndexTemplate tasks={tasks} />;
+      }}
+    </Query>
+  );
+};
+
+export default TaskIndexPage;
