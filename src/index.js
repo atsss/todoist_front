8a97e2b68700b7fc2acb3e6ed8e07488eb26489c
atsushi0623itoh <atsushi0623itoh@gmail.com>
2019-09-27 20:47:32 +0900

    rename constants.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { SERVER_ENDPOINT } from "./constants";
+import { SERVER_ENDPOINT } from "./variables/endpoint";
 import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 import { ApolloProvider } from "react-apollo";
