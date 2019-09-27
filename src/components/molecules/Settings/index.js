9002631947af6d93a0f6b2d891e7a9f9baa8da8e
atsushi0623itoh <atsushi0623itoh@gmail.com>
2019-09-27 21:04:23 +0900

    use useState instead of unstated provider

--- src/components/molecules/Settings/index.js
+++ src/components/molecules/Settings/index.js
@@ -1,48 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles.module.sass";
 import Link from "../../atoms/Link";
 import Txt from "../../atoms/Txt";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsisH } from "@fortawesome/free-solid-svg-icons";
-import { SettingsContainer as DisplayContainer } from "../../../containers";
 import { containerPresenter } from "../../utils/HoC.js";
 
-const SettingsPresenter = ({ id, className }) => {
-  let settings = DisplayContainer.useContainer();
-  return (
-    <div className={[styles.root, className].join(" ")}>
-      <FontAwesomeIcon
-        className={styles.icon}
-        icon={faEllipsisH}
-        onClick={settings.toggle}
-      />
-      <ul
-        className={
-          settings.display
-            ? [styles.menu, styles.active].join(" ")
-            : styles.menu
-        }
-      >
-        <li>
-          <Link to={`/tasks/${id}/edit`}>
-            <Txt>タスクを編集</Txt>
-          </Link>
-        </li>
-        <li>
-          <Link to={`/tasks/${id}/edit`}>
-            <Txt className="u-mt10">タスクを削除</Txt>
-          </Link>
-        </li>
-      </ul>
-    </div>
-  );
-};
-
-const SettingsContainer = ({ presenter, ...props }) => (
-  <DisplayContainer.Provider>
-    {presenter({ ...props })}
-  </DisplayContainer.Provider>
+const SettingsPresenter = ({ id, isDisplay, toggle, className }) => (
+  <div className={[styles.root, className].join(" ")}>
+    <FontAwesomeIcon
+      className={styles.icon}
+      icon={faEllipsisH}
+      onClick={toggle}
+    />
+    <ul
+      className={
+        isDisplay ? [styles.menu, styles.active].join(" ") : styles.menu
+      }
+    >
+      <li>
+        <Link to={`/tasks/${id}/edit`}>
+          <Txt>タスクを編集</Txt>
+        </Link>
+      </li>
+      <li>
+        <Link to={`/tasks/${id}/edit`}>
+          <Txt className="u-mt10">タスクを削除</Txt>
+        </Link>
+      </li>
+    </ul>
+  </div>
 );
+
+const SettingsContainer = ({ presenter, ...props }) => {
+  const [isDisplay, setIsDisplay] = useState(false);
+  const toggle = () => setIsDisplay(!isDisplay);
+
+  return presenter({ isDisplay, toggle, ...props });
+};
 
 const Settings = containerPresenter(SettingsContainer, SettingsPresenter);
 
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,14 +1,6 @@
 import { useState } from "react";
 import { createContainer } from "unstated-next";
 
-function useSettings(initialState = false) {
-  let [display, setDisplay] = useState(initialState);
-  let toggle = () => setDisplay(!display);
-  return { display, toggle };
-}
-
-export const SettingsContainer = createContainer(useSettings);
-
 function useSession(initialState = false) {
   let [isLogin, setSession] = useState(initialState);
   let login = () => setSession(true);
