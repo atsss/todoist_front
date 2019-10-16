3701d3fd126348870397cdcfc55f1a09e0e7e9cd
atsushi0623itoh <atsushi0623itoh@gmail.com>
2019-10-16 12:07:48 +0900

    add auth information to request header

--- src/index.js
+++ src/index.js
@@ -7,16 +7,29 @@ import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
+import { ApolloLink } from "apollo-link";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { SessionContainer } from "./containers";
+import { CLIENT, UID, ACCESS_TOKEN } from "./variables/localStrageKey";
 
 const httpLink = createHttpLink({
   uri: SERVER_ENDPOINT
 });
 
+const middlewareLink = new ApolloLink((operation, forward) => {
+  operation.setContext({
+    headers: {
+      CLIENT: localStorage.getItem(CLIENT) || null,
+      UID: localStorage.getItem(UID) || null,
+      ACCESS_TOKEN: localStorage.getItem(ACCESS_TOKEN) || null
+    }
+  });
+  return forward(operation);
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: middlewareLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
