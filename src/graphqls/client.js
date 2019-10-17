7ad2d7a0ba2a319ad41d4ad9531c88fc204f399a
atsushi0623itoh <atsushi0623itoh@gmail.com>
2019-10-17 20:50:25 +0900

    separete apollo client from index.js

---
+++ src/graphqls/client.js
@@ -0,0 +1,28 @@
+import { ApolloClient } from "apollo-client";
+import { ApolloLink } from "apollo-link";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { CLIENT, UID, ACCESS_TOKEN } from "../variables/localStrageKey";
+import { SERVER_ENDPOINT } from "../variables/endpoint";
+
+const httpLink = createHttpLink({
+  uri: SERVER_ENDPOINT
+});
+
+const middlewareLink = new ApolloLink((operation, forward) => {
+  operation.setContext({
+    headers: {
+      CLIENT: localStorage.getItem(CLIENT) || null,
+      UID: localStorage.getItem(UID) || null,
+      ACCESS_TOKEN: localStorage.getItem(ACCESS_TOKEN) || null
+    }
+  });
+  return forward(operation);
+});
+
+const client = new ApolloClient({
+  link: middlewareLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
+export default client;
--- src/index.js
+++ src/index.js
@@ -2,44 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { SERVER_ENDPOINT } from "./variables/endpoint";
 import { BrowserRouter } from "react-router-dom";
-import * as serviceWorker from "./serviceWorker";
 import { ApolloProvider } from "react-apollo";
-import { ApolloClient } from "apollo-client";
-import { ApolloLink } from "apollo-link";
-import { createHttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
+import * as serviceWorker from "./serviceWorker";
 import { SessionContainer } from "./containers";
-import { CLIENT, UID, ACCESS_TOKEN } from "./variables/localStrageKey";
-
-const httpLink = createHttpLink({
-  uri: SERVER_ENDPOINT
-});
-
-const middlewareLink = new ApolloLink((operation, forward) => {
-  operation.setContext({
-    headers: {
-      CLIENT: localStorage.getItem(CLIENT) || null,
-      UID: localStorage.getItem(UID) || null,
-      ACCESS_TOKEN: localStorage.getItem(ACCESS_TOKEN) || null
-    }
-  });
-  return forward(operation);
-});
-
-const client = new ApolloClient({
-  link: middlewareLink.concat(httpLink),
-  cache: new InMemoryCache()
-});
+import client from "./graphqls/client";
 
 ReactDOM.render(
   <BrowserRouter>
-    <ApolloProvider client={client}>
-      <SessionContainer.Provider>
+    <SessionContainer.Provider>
+      <ApolloProvider client={client}>
         <App />
-      </SessionContainer.Provider>
-    </ApolloProvider>
+      </ApolloProvider>
+    </SessionContainer.Provider>
   </BrowserRouter>,
   document.getElementById("root")
 );
