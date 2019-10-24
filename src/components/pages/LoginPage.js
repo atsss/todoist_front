806a8e1949aae3379283c13de94a53f17a3c79b1
atsushi0623itoh <atsushi0623itoh@gmail.com>
2019-10-24 22:35:01 +0900

    redirect to top page if login

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,6 +1,18 @@
 import React from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import { SessionContainer } from "../../containers";
 import LoginTemplate from "../templates/Login";
 
-const LoginPage = () => <LoginTemplate />;
+const LoginPage = () => {
+  const session = SessionContainer.useContainer();
+  const history = useHistory();
+  const location = useLocation();
+
+  const { from } = location.state || { from: { pathname: "/" } };
+
+  if (session.isLogin) history.replace(from);
+
+  return <LoginTemplate />;
+};
 
 export default LoginPage;
